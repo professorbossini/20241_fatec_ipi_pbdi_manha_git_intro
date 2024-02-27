34d9783fb1d5febc1496b097dfc5ca0e5780712d
Rodrigo Bossini <professorbossini@gmail.com>
2024-02-27 10:16:37 -0300

    test(git-python): funcao divide teste

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -5,8 +5,10 @@ def main():
   soma = calculadora.somar(a, b)
   subtracao = calculadora.subtrair(a, b)
   produto = calculadora.multiplicar(a, b)
+  quociente = calculadora.dividir(a, b)
   print(f'{a} + {b} = {soma}')
   print(f'{a} - {b} = {subtracao}')
   print(f'{a} * {b} = {produto}')
+  print(f'{a} / {b} = {quociente}')
 
 main()
