8ad8ed7032863d09145db1feb673ff615bffde83
Rodrigo Bossini <professorbossini@gmail.com>
2024-02-27 09:13:20 -0300

    test(git-python): funcao subtrai teste

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -3,6 +3,8 @@ def main():
   a = 2
   b = 3
   soma = calculadora.somar(a, b)
+  subtracao = calculadora.subtrair(a, b)
   print(f'{a} + {b} = {soma}')
+  print(f'{a} - {b} = {subtracao}')
 
 main()
